e28f923496142f57092e2f6840b4b3ddebcd9c71
marie-hotchkiss <mtknowlton67@gmail.com>
2020-07-14 16:34:10 -0700

    added router.get

--- data/db-router.js
+++ data/db-router.js
@@ -16,3 +16,15 @@ router.get('/', (req, res) => {
         res.status(500).json({ error: "The posts information could not be retrieved." })
     });
 });
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id)
+    .then(posts => {
+        posts ? res.status(200).json(posts) :
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+    .catch(error => {
+        console.log('error from GET/:id', error);
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    });
+});
