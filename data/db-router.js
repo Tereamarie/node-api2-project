4a390dd759fd4cc2e04f6ab852844ad5c8f6d510
marie-hotchkiss <mtknowlton67@gmail.com>
2020-07-14 16:36:09 -0700

    Added Post request

--- data/db-router.js
+++ data/db-router.js
@@ -28,3 +28,64 @@ router.get('/:id', (req, res) => {
         res.status(500).json({ error: "The post information could not be retrieved." })
     });
 });
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+    .then(comment => {
+        !comment ? res.status(500).json({ message: "The post with the specified ID does not exist." }) : res.status(200).json(comment);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+    })
+})
+//POST requests
+
+router.post('/', (req, res) => {
+    const {title, contents} = Posts.insert(req.body)
+    .then(posts => {
+       title || contents  ?
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post."}) : res.status(201).json(posts);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    });
+
+});
+
+router.post("/:id/comments", (req, res) => {
+    const id = req.params.id;
+    const { text } = req.body;
+    if (text) {
+    Posts.findById(id)
+    .then(post => {
+        if (post.length) {
+        Posts.insertComment({ text: text, post_id: id })
+            .then(() => {
+            res.status(201).json({ message: req.body });
+            })
+            .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:
+                "There was an error while saving the comment to the database."
+            });
+            });
+        } else {
+        res.status(404).json({
+            message: "The post with the specified ID does not exist."
+        });
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+        error: "There was an error while saving the comment to the database."
+        });
+    });
+} else {
+    res
+    .status(400)
+    .json({ errorMessage: "Please provide text for the comment." });
+}
+});
